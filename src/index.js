0d3fcb7d60ebe724d45f6dcea6a5080001f3c902
Hannah Barton <hannbarton@gmail.com>
2018-10-07 15:57:38 -0400

    working files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import NavBar from "./NavBar.js";
+import { Parallax } from 'react-spring'
 
-const title = 'My Minimal React Webpack Babel Setup';
+// http://ap.imagensbrasil.org/images/site-background.jpg
 
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
+class Main extends Component {
+  render() {
+    return (
+      <div className='container'>
+				<h2>hello world</h2>
+				<NavBar />
+
+        <Parallax>
+          <Parallax.Layer offset={1} speed={1} style={{ backgroundColor: '#805E73' }}>
+
+          </Parallax.Layer>
+
+        </Parallax>
+     </div>
+
+
+    //     <Parallax ref={ref => (this.parallax = ref)} pages={3}>
+    //     <Parallax.Layer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} > </Parallax.Layer>
+		//  </Parallax><
+		)
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById("app"));
 
 module.hot.accept();
